171671cc094c3c30a5f8ab8e1690054402d0a073
Romain Mullot <romain.mullot@lilo.org>
2019-01-22 12:11:58 +0100

    Updated pod Creation of WeatherCore Added code to detect network

---
+++ WeatherSwift/WeatherCore/Network/MultiCastDelegate.swift
@@ -0,0 +1,48 @@
+//
+//  MultiCastDelegate.swift
+//  RMCore
+//
+//  Created by Romain Mullot on 22/01/2019.
+//  Copyright © 2018 Romain Mullot. All rights reserved.
+//
+
+import Foundation
+
+public final class MulticastDelegate <T> {
+
+  private let lock: PThreadMutex = PThreadMutex()
+
+  private let delegates: NSHashTable<AnyObject> = NSHashTable.weakObjects()
+
+  fileprivate let addClosure: ((T) -> Void)?
+
+  public init(addClosure: ((T) -> Void)? = nil) {
+      self.addClosure = addClosure
+  }
+
+  public func add(_ delegate: T) {
+      self.lock.sync {
+          delegates.add(delegate as AnyObject)
+          self.addClosure?(delegate)
+      }
+  }
+
+  public func remove(_ delegate: T) {
+      self.lock.sync {
+          for oneDelegate in delegates.allObjects.reversed() where oneDelegate === delegate as AnyObject {
+              delegates.remove(oneDelegate)
+          }
+      }
+  }
+
+  public func invoke(_ invocation: (T) -> Void) {
+      self.lock.sync {
+          for delegate in delegates.allObjects.reversed() {
+              if let delegate = delegate as? T {
+                  invocation(delegate)
+              }
+          }
+      }
+  }
+
+}
---
+++ WeatherSwift/WeatherCore/WeatherCore.h
@@ -0,0 +1,19 @@
+//
+//  WeatherCore.h
+//  WeatherCore
+//
+//  Created by Romain Mullot on 22/01/2019.
+//  Copyright © 2019 Romain Mullot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WeatherCore.
+FOUNDATION_EXPORT double WeatherCoreVersionNumber;
+
+//! Project version string for WeatherCore.
+FOUNDATION_EXPORT const unsigned char WeatherCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WeatherCore/PublicHeader.h>
+
+
