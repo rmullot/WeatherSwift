e3300e10fa1f04d3746569aaa54e7487a7c29a36
Romain Mullot <romainmullot@free.fr>
2019-02-04 14:31:38 +0100

    Resolved bug references

--- WeatherSwift/WeatherCore/Models/Fetchable/Forecast+CoreDataProperties.swift
+++ WeatherSwift/WeatherCore/Models/Fetchable/Forecast+CoreDataProperties.swift
@@ -1,9 +1,9 @@
 //
 //  Forecast+CoreDataProperties.swift
-//  
+//  WeatherSwift
 //
-//  Created by Romain Mullot on 03/02/2019.
-//
+//  Created by Romain Mullot on 18/11/2018.
+//  Copyright © 2018 Romain Mullot. All rights reserved.
 //
 
 import Foundation
@@ -15,16 +15,16 @@ extension Forecast {
     return NSFetchRequest<Forecast>(entityName: "Forecast")
   }
 
-  @NSManaged public var date: Double
-  @NSManaged public var informations: String?
-  @NSManaged public var idImage: String?
-  @NSManaged public var temperature: Double
   @NSManaged public var clouds: Float
+  @NSManaged public var date: Double
+  @NSManaged public var humidity: Int16
+  @NSManaged public var idImage: String?
+  @NSManaged public var informations: String?
+  @NSManaged public var pressure: Float
   @NSManaged public var rain: Float
   @NSManaged public var snow: Float
-  @NSManaged public var pressure: Float
-  @NSManaged public var humidity: Int16
   @NSManaged public var speed: Float
+  @NSManaged public var temperature: Double
   @NSManaged public var winds: Float
 
 }
