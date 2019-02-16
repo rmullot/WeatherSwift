bfd1243e7bd91cc30c9fc1bdcf75877db61d4d0f
Romain Mullot <romainmullot@free.fr>
2019-02-16 10:09:52 +0100

    updated git ignore Added new Forecast+CoreDataClass

--- WeatherSwift/WeatherCore/Models/Fetchable/Forecast+CoreDataProperties.swift
+++ WeatherSwift/WeatherCore/Models/Fetchable/Forecast+CoreDataProperties.swift
@@ -1,9 +1,9 @@
 //
 //  Forecast+CoreDataProperties.swift
-//  WeatherSwift
+//  
 //
-//  Created by Romain Mullot on 18/11/2018.
-//  Copyright © 2018 Romain Mullot. All rights reserved.
+//  Created by Romain Mullot on 15/02/2019.
+//
 //
 
 import Foundation
@@ -11,20 +11,20 @@ import CoreData
 
 extension Forecast {
 
-  @nonobjc public class func fetchRequest() -> NSFetchRequest<Forecast> {
-    return NSFetchRequest<Forecast>(entityName: "Forecast")
-  }
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Forecast> {
+        return NSFetchRequest<Forecast>(entityName: "Forecast")
+    }
 
-  @NSManaged public var clouds: Float
-  @NSManaged public var date: Double
-  @NSManaged public var humidity: Int16
-  @NSManaged public var idImage: String?
-  @NSManaged public var informations: String?
-  @NSManaged public var pressure: Float
-  @NSManaged public var rain: Float
-  @NSManaged public var snow: Float
-  @NSManaged public var speed: Float
-  @NSManaged public var temperature: Double
-  @NSManaged public var winds: Float
+    @NSManaged public var clouds: Float
+    @NSManaged public var date: Double
+    @NSManaged public var humidity: Int16
+    @NSManaged public var idImage: String?
+    @NSManaged public var informations: String?
+    @NSManaged public var pressure: Float
+    @NSManaged public var rain: Float
+    @NSManaged public var snow: Float
+    @NSManaged public var speed: Float
+    @NSManaged public var temperature: Double
+    @NSManaged public var winds: Float
 
 }
